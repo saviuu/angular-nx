373147a238e8ff68a2e56cceff90f4a7fd145597
Sávio Bertoldi <savio.b09@gmail.com>
2023-08-29 21:07:38 -0300

    feat: add modules

---
+++ modules/products/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/products.module';
+export * from './lib/lib.routes';
+
+export * from './lib/lib.routes';
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent, HelloWorldComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
